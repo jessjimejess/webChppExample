6c295985bcff4455469c470bf10017af29814fc9
jessjimejess <jessjimejess@gmail.com>
2020-11-02 16:42:30 +0100

    App acabada

--- webChapp/webChapp/settings.py
+++ webChapp/webChapp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u6b8&-xzq(xu3k00nhjw@1jh&)dana#vc4@&bdaxe)93se9=t%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
